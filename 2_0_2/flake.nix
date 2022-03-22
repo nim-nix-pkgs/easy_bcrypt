cdd55c3213b2bbbef61e26fd87b20447f7c04f8b
hugosenari <hugosenari@gmail.com>
2022-03-22 04:29:09 -0300

    chore: re index easy_bcrypt

--- 2_0_2/flake.nix
+++ 2_0_2/flake.nix
@@ -10,7 +10,7 @@
   inputs.src-easy_bcrypt-2_0_2.flake = false;
   inputs.src-easy_bcrypt-2_0_2.ref   = "refs/tags/2.0.2";
   inputs.src-easy_bcrypt-2_0_2.owner = "Akito13";
-  inputs.src-easy_bcrypt-2_0_2.repo  = "easy-bcrypt.git";
+  inputs.src-easy_bcrypt-2_0_2.repo  = "easy-bcrypt";
   inputs.src-easy_bcrypt-2_0_2.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
